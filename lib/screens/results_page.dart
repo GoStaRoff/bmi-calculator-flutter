5962bff1c8288ac99eb33346fcd547927006007c
Oleksandr <gostar1337@gmail.com>
2021-04-08 17:55:57 +0300

    Section Completed

---
+++ lib/screens/results_page.dart
@@ -0,0 +1,68 @@
+import 'package:bmi_calculator/calculator_brain.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/reuseable_card.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  final String interpretation;
+  final String resultText;
+  final String bmiResult;
+
+  ResultsPage({this.interpretation, this.bmiResult, this.resultText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Your results',
+                style: kTittleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              color: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation,
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            flex: 5,
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
